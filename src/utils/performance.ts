c7cf20175ece5028f3dd1f7f7c82aed7a4916c24
hxf <hongxin.feng@transwarp.io>
2025-12-06 20:14:51 +0800

    feat: again

--- src/utils/performance.ts
+++ src/utils/performance.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: hxf hongxin.feng@transwarp.io
+ * @Date: 2025-12-06 20:06:39
+ * @LastEditors: hxf hongxin.feng@transwarp.io
+ * @LastEditTime: 2025-12-06 20:14:15
+ * @FilePath: \my-app\src\utils\performance.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // src/utils/performance.ts
 
 /**
